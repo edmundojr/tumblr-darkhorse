2b648689471c108abe8ede967bb046ea44c2f19b
Edmundo Santos <hello@edmundojr.com>
2016-03-15 17:29:04 +0000

    Better gulpfile with options object

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// *************************************
+//  Gulpfile
+// *************************************
+
 'use strict';
 
 var gulp         = require('gulp');
@@ -6,31 +10,66 @@ var autoprefixer = require('gulp-autoprefixer');
 var uglify       = require('gulp-uglify');
 var browserSync  = require('browser-sync');
 
-// Static Server / Watching files
+// -------------------------------------
+//  Options
+// -------------------------------------
+
+var options = {
+
+	html: {
+		files       : '*.html'
+	},
+
+	sass: {
+		files	    : 'src/scss/**/*.scss',
+		destination : 'dist/css'
+	},
+
+	scripts: {
+		files       : 'src/js/**/*.js',
+		destination : 'dist/js'
+	}
+};
+
+// -------------------------------------
+//  Task: Serve
+// -------------------------------------
+
 gulp.task('serve', ['styles', 'scripts'], function() {
 	browserSync.init({
 		server: './'
 	});
 
-	gulp.watch('src/scss/**/*.scss', ['styles']);
-	gulp.watch('*.html').on('change', browserSync.reload);
+	gulp.watch(options.sass.files, ['styles']);
+	gulp.watch(options.scripts.files).on('change', browserSync.reload);
+	gulp.watch(options.html.files).on('change', browserSync.reload);
 });
 
-// Compile sass into CSS
+// -------------------------------------
+//  Task: Sass
+// -------------------------------------
+
 gulp.task('styles', function() {
-	return gulp.src('src/scss/**/*.scss')
-		.pipe(sass({outputStyle: 'compressed'}))
-		.pipe(autoprefixer('last 2 versions', '> 2%'))
-		.pipe(gulp.dest('dist/css/'))
+	return gulp.src(options.sass.files)
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer('last 3 versions'))
+		.pipe(gulp.dest(options.sass.destination))
 		.pipe(browserSync.stream());
 });
 
-// Minify JS files
+// -------------------------------------
+//  Task: Scrits
+// -------------------------------------
+
 gulp.task('scripts', function() {
-	return gulp.src('src/js/*.js')
+	return gulp.src(options.scripts.files)
 		.pipe(uglify())
-		.pipe(gulp.dest('dist/js/'))
+		.pipe(gulp.dest(options.scripts.destination))
 		.pipe(browserSync.stream());
 });
+
+// -------------------------------------
+//  Task: Default
+// -------------------------------------
 
 gulp.task('default', ['serve']);
